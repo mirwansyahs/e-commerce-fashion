eda1a1fac0f2fa35f5cb10455011f31060bbe1b1
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-07-26 15:32:45 +0700

    update

--- app/Views/layout/back-end/sidebar.php
+++ app/Views/layout/back-end/sidebar.php
@@ -1,5 +1,6 @@
     <?php 
         $db = db_connect();
+        $request = \Config\Services::request();
         $query = $db->table('store')->getWhere([])->getRow(); 
     ?>
     <div class="main-sidebar sidebar-style-2">
@@ -13,25 +14,46 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class=active><a class="nav-link" href="dashboard"><i class="fa fa-fire"></i> <span>Dashboard</span></a></li>
+            <li class="<?= $request->uri->getSegment(1) == 'dashboard' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="dashboard"><i class="fa fa-fire"></i> <span>Dashboard</span></a>
+            </li>
             <li class="menu-header">Menu</li>
-            <li class=""><a class="nav-link" href="kategori"><i class="fas fa-th"></i> <span>Kategori</span></a></li>
-            <li class=""><a class="nav-link" href="produk"><i class="fas fa-tshirt"></i> <span>Produk</span></a></li>
-            <li class=""><a class="nav-link" href="diskon"><i class="fas fa-percent"></i> <span>Diskon</span></a></li>
-            <li class="dropdown">
+            <li class="<?= $request->uri->getSegment(1) == 'kategori' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="kategori"><i class="fas fa-th"></i> <span>Kategori</span></a>
+            </li>
+            <li class="<?= $request->uri->getSegment(1) == 'produk' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="produk"><i class="fas fa-tshirt"></i> <span>Produk</span></a>
+            </li>
+            <li class="<?= $request->uri->getSegment(1) == 'diskon' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="diskon"><i class="fas fa-percent"></i> <span>Diskon</span></a>
+            </li>
+            <li class="dropdown <?= $request->uri->getSegment(1) == 'order-masuk' || $request->uri->getSegment(1) == 'order-selesai' ? 'active' : ''?>">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-shopping-bag"></i> <span>Order</span></a>
             <ul class="dropdown-menu">              
-                <li><a class="nav-link" href="order-masuk">Order Masuk</a></li>                
-                <li><a class="nav-link" href="order-selesai">Order Selesai</a></li>                
+                <li class="<?= $request->uri->getSegment(1) == 'order-masuk' ? 'active' : ''?>">
+                    <a class="nav-link" href="order-masuk">Order Masuk</a>
+                </li>                
+                <li class="<?= $request->uri->getSegment(1) == 'order-selesai' ? 'active' : ''?>">
+                    <a class="nav-link" href="order-selesai">Order Selesai</a>
+                </li>                
             </ul>
             </li>
-            <li class=""><a class="nav-link" href="pelanggan"><i class="fas fa-user"></i> <span>Pelanggan</span></a></li>
-            <li class=""><a class="nav-link" href="chat"><i class="fas fa-comment-alt"></i> <span>Chat</span></a></li>
+            <li class="<?= $request->uri->getSegment(1) == 'pelanggan' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="pelanggan"><i class="fas fa-user"></i> <span>Pelanggan</span></a>
+            </li>
+            <li class="<?= $request->uri->getSegment(1) == 'chat' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="chat"><i class="fas fa-comment-alt"></i> <span>Chat</span></a>
             </li>
             <li class="menu-header">Pengaturan</li>
-            <li class=""><a class="nav-link" href="profil"><i class="far fa-user"></i> <span>Profil</span></a></li>
-            <li class=""><a class="nav-link" href="ubah-kata-sandi"><i class="fas fa-key"></i> <span>Ubah Kata Sandi</span></a></li>
-            <li class=""><a class="nav-link" href="pengaturan"><i class="fas fa-cog"></i></i> <span>Pengaturan</span></a></li>
+            <li class="<?= $request->uri->getSegment(1) == 'profil' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="profil"><i class="far fa-user"></i> <span>Profil</span></a>
+            </li>
+            <li class="<?= $request->uri->getSegment(1) == 'ubah-kata-sandi' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="ubah-kata-sandi"><i class="fas fa-key"></i> <span>Ubah Kata Sandi</span></a>
+            </li>
+            <li class="<?= $request->uri->getSegment(1) == 'pengaturan' || $request->uri->getSegment(1) == '' ? 'active' : ''?>">
+                <a class="nav-link" href="pengaturan"><i class="fas fa-cog"></i></i> <span>Pengaturan</span></a>
+            </li>
             <li class="mb-4"><a class="nav-link" href="keluar"><i class="fas fa-sign-out-alt"></i></i> <span>Keluar</span></a></li>
         </ul>     
     </aside>
